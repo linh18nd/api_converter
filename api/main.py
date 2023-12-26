0896b4a8aa47c56e07319562f09984acb3853988
linh18nd <linhth1@kaopiz.com>
2023-12-26 14:56:05 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -183,17 +183,14 @@ def get_all_documents(api_key: APIKey = Depends(check_api_key)):
 @app.delete("/ocr/{pid}")
 def delete_doc(pid: UUID, api_key: APIKey = Depends(check_api_key)):
     if pid in documents:
-        # Xoá tệp đầu ra và các tệp liên quan khác nếu cần
         output_doc = documents[pid].output
         output_doc_json = documents[pid].output_json
         output_doc_txt = documents[pid].output_txt
 
-        # Kiểm tra và xoá tệp nếu tồn tại
         for file_path in [output_doc, output_doc_json, output_doc_txt]:
             if file_path.resolve().exists():
                 file_path.unlink()
 
-        # Xoá tài liệu khỏi danh sách
         del documents[pid]
 
         return {"status": "success", "message": f"Document {pid} deleted successfully"}
